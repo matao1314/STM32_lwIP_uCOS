17a3d7fcb4cb388d67c5e6574a15e7d36fd0fb93
matao <1585641394@qq.com>
2013-12-19 22:57:29 +0800

    add lwip libiary

--- USER/main.c
+++ USER/main.c
@@ -1,6 +1,13 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
 #include "sys.h"
+#include "usart.h"		
+#include "delay.h"	
+//Extend api
+#include "lcd.h"
+#include "led.h"
+#include "key.h"
+
 /**
   * @brief  Main program.
   * @param  None
@@ -9,10 +16,19 @@
 int main(void)
 {
   /* Add your application code here
-     */
-
+     */				 
+	SystemInit();
+	uart_init(72,9600);	 
+	delay_init(72);	   	
+	LED_Init();
+	KEY_init();
+  LCD_Init();
   /* Infinite loop */
   while (1)
   {
+
+
+	LCD_Clear(RED);
   }
 }
+
